25eb1a1641db04f98e9548c0d59684ddbfed6fa7
Hina263 <hinato263@gmail.com>
2026-09-17 10:16:36 +0900

    Initial Xcode project setup

---
+++ NAGORI/NAGORI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NAGORI
+//
+//  Created by 長野日向人 on 2026/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NAGORI/NAGORI/NAGORIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAGORIApp.swift
+//  NAGORI
+//
+//  Created by 長野日向人 on 2026/09/17.
+//
+
+import SwiftUI
+
+@main
+struct NAGORIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
